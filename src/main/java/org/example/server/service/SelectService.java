712115c809765c1667cde9c86d60fb45dc383893
Silviu Stoian <silviu_stoian25@yahoo.com>
2023-12-04 18:14:44 +0200

    Use index files for range queries also

--- src/main/java/org/example/server/service/SelectService.java
+++ src/main/java/org/example/server/service/SelectService.java
@@ -1,16 +1,18 @@
 package org.example.server.service;
 
-import com.mongodb.client.AggregateIterable;
+
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.expression.LongValue;
+import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
 import net.sf.jsqlparser.expression.operators.relational.ComparisonOperator;
 import net.sf.jsqlparser.schema.Column;
 import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
 import net.sf.jsqlparser.statement.select.SelectVisitorAdapter;
+import org.bson.BsonTimestamp;
 import org.example.server.repository.DBMSRepository;
 import org.jdom2.Element;
 import org.bson.Document;
@@ -19,8 +21,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 
-import static com.mongodb.client.model.Filters.expr;
-import static com.mongodb.client.model.Filters.gt;
 import static org.example.server.connectionManager.DbConnectionManager.getMongoClient;
 
 public class SelectService {
@@ -31,38 +31,102 @@ public class SelectService {
         this.dbmsRepository = dbmsRepository;
     }
 
-    public void processSelect(Select selectStatement) {
+    public void processSelect(Select selectStatement) throws Exception {
         Element rootDataBases = dbmsRepository.getDoc().getRootElement();
         MongoClient client = getMongoClient();
         MongoDatabase database = client.getDatabase(DATABASE_NAME);
-        selectStatement.getSelectBody().accept(new SelectVisitorAdapter() {
-            @Override
-            public void visit(PlainSelect plainSelect) {
-                String tableName = plainSelect.getFromItem().toString(); // Obținem numele tabelei
-                Expression where = plainSelect.getWhere(); // Obținem clauza WHERE
 
-                if (where instanceof ComparisonOperator) {
-                    ComparisonOperator comparisonOperator = (ComparisonOperator) where;
-                    Column column = (Column) comparisonOperator.getLeftExpression();
-                    String columnName = column.getColumnName();
+        PlainSelect plainSelect = (PlainSelect) selectStatement.getSelectBody();
 
-                    LongValue longValue = (LongValue) comparisonOperator.getRightExpression();
-                    int conditionValue = (int) longValue.getValue();
+        String tableName = plainSelect.getFromItem().toString(); // Obținem numele tabelei
+        Expression where = plainSelect.getWhere(); // Obținem clauza WHERE
 
-                    String operator = comparisonOperator.getStringExpression();
-                    if (existIndexForColumn(rootDataBases, tableName, columnName)) {
-                        MongoCollection<Document> studentsCollection = database.getCollection(dbmsRepository.getCurrentDatabase() + tableName);
-                        MongoCollection<Document> indexCollection = database.getCollection(dbmsRepository.getCurrentDatabase() + tableName + "Ind" + columnName);
-                        List<String> matchingIDs = findMatchingIDs(indexCollection, columnName, conditionValue, operator);
-                        List<String> result = retrieveEntities(studentsCollection, matchingIDs);
-                        for (String entity : result) {
-                            System.out.println(entity);
-                        }
-                    }
+        if (where instanceof ComparisonOperator) {
+            processSingleCondition(rootDataBases, database, tableName, where);
+
+        } else if (where instanceof AndExpression) {
+            // Tratează cazul în care există mai multe condiții legate prin AND în clauza WHERE
+            List<Expression> conditions = extractMultipleConditions((AndExpression) where);
+            processMultipleConditions(rootDataBases, database, tableName, conditions);
+        }
+    }
+
+
+    private List<Expression> extractMultipleConditions(AndExpression andExpression) {
+        List<Expression> conditions = new ArrayList<>();
+        Expression left = andExpression.getLeftExpression();
+        Expression right = andExpression.getRightExpression();
+
+        if (left instanceof AndExpression) {
+            conditions.addAll(extractMultipleConditions((AndExpression) left));
+        } else {
+            conditions.add(left);
+        }
+
+        if (right instanceof AndExpression) {
+            conditions.addAll(extractMultipleConditions((AndExpression) right));
+        } else {
+            conditions.add(right);
+        }
+
+        return conditions;
+    }
+
+    private void processSingleCondition(Element rootDataBases, MongoDatabase database, String tableName, Expression where) throws Exception {
+        ComparisonOperator comparisonOperator = (ComparisonOperator) where;
+        Column column = (Column) comparisonOperator.getLeftExpression();
+        String columnName = column.getColumnName();
+
+        LongValue longValue = (LongValue) comparisonOperator.getRightExpression();
+        int conditionValue = (int) longValue.getValue();
+
+        List<String> matchingIDs = List.of();
+        String operator = comparisonOperator.getStringExpression();
+        if (existIndexForColumn(rootDataBases, tableName, columnName)) {
+            MongoCollection<Document> indexCollection = database.getCollection(dbmsRepository.getCurrentDatabase() + tableName + "Ind" + columnName);
+            matchingIDs = findMatchingIDs(indexCollection, columnName, conditionValue, operator);
+        }
+        if (!matchingIDs.isEmpty()) {
+            MongoCollection<Document> studentsCollection = database.getCollection(dbmsRepository.getCurrentDatabase() + tableName);
+            List<String> result = retrieveEntities(studentsCollection, matchingIDs);
+            result.forEach(System.out::println);
+        } else {
+            System.out.println("Nu au fost găsite înregistrări care să satisfacă toate condițiile.");
+        }
+    }
+
+    private void processMultipleConditions(Element rootDataBases, MongoDatabase database, String tableName, List<Expression> conditions) throws Exception {
+        List<String> allMatchingIDs = new ArrayList<>();
+
+        for (Expression condition : conditions) {
+            if (condition instanceof ComparisonOperator) {
+                ComparisonOperator comparisonOperator = (ComparisonOperator) condition;
+                Column column = (Column) comparisonOperator.getLeftExpression();
+                String columnName = column.getColumnName();
+
+                LongValue longValue = (LongValue) comparisonOperator.getRightExpression();
+                int conditionValue = (int) longValue.getValue();
+
+                String operator = comparisonOperator.getStringExpression();
+                if (existIndexForColumn(rootDataBases, tableName, columnName)) {
+                    MongoCollection<Document> indexCollection = database.getCollection(dbmsRepository.getCurrentDatabase() + tableName + "Ind" + columnName);
+                    List<String> matchingIDs = findMatchingIDs(indexCollection, columnName, conditionValue, operator);
+                    if (allMatchingIDs.isEmpty())
+                        allMatchingIDs.addAll(matchingIDs);
+                    else
+                        allMatchingIDs.retainAll(matchingIDs);
                 }
             }
-        });
+        }
 
+        if (!allMatchingIDs.isEmpty()) {
+
+            MongoCollection<Document> studentsCollection = database.getCollection(dbmsRepository.getCurrentDatabase() + tableName);
+            List<String> result = retrieveEntities(studentsCollection, allMatchingIDs);
+            result.forEach(System.out::println);
+        } else {
+            System.out.println("Nu au fost găsite înregistrări care să satisfacă toate condițiile.");
+        }
     }
 
     // Funcție pentru a verifica dacă există un index pentru o anumită coloană
@@ -74,10 +138,12 @@ public class SelectService {
         Element table = rootDataBases.getChild("DataBase").getChild("Table");
         String tableAttrName = table.getAttributeValue("tableName");
         if (tableAttrName.equals(tableName)) {
-            Element indexFiles = table.getChild("IndexFiles");
-            Element indexFile = indexFiles.getChild("IndexFile");
-            String indexAttrName = indexFile.getChild("IndexAttributes").getChildText("IAttribute");
-            return indexAttrName.equals(columnName);
+            List<Element> indexFiles = table.getChild("IndexFiles").getChildren();
+            for (Element indexFile : indexFiles) {
+                String indexAttrName = indexFile.getChild("IndexAttributes").getChildText("IAttribute");
+                if(indexAttrName.equals(columnName))
+                    return true;
+            }
         }
         return false;
     }
@@ -110,9 +176,8 @@ public class SelectService {
                 // Poți trata această situație în funcție de necesitățile tale
                 break;
         }
-        System.out.println(query.toJson());
         for (Document document : indexCollection.find(query)) {
-            matchingIDs.add(document.get("values").toString());
+            matchingIDs.addAll(Arrays.asList(document.get("values").toString().split("\\$")));
         }
 
         return matchingIDs;
