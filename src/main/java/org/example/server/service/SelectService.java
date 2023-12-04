831a1fcd775c0408e8d7b4ba5171e4fe588c7f42
Silviu Stoian <silviu_stoian25@yahoo.com>
2023-12-04 21:36:09 +0200

    Refactoring the code

--- src/main/java/org/example/server/service/SelectService.java
+++ src/main/java/org/example/server/service/SelectService.java
@@ -5,19 +5,15 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import net.sf.jsqlparser.expression.Expression;
-import net.sf.jsqlparser.expression.LongValue;
 import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
 import net.sf.jsqlparser.expression.operators.relational.ComparisonOperator;
 import net.sf.jsqlparser.expression.operators.relational.LikeExpression;
 import net.sf.jsqlparser.schema.Column;
 import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
-import net.sf.jsqlparser.statement.select.SelectVisitorAdapter;
-import org.bson.BsonTimestamp;
 import org.example.server.repository.DBMSRepository;
 import org.jdom2.Element;
 import org.bson.Document;
-import org.jdom2.Parent;
 
 import java.util.Arrays;
 import java.util.List;
@@ -44,107 +40,41 @@ public class SelectService {
         String tableName = plainSelect.getFromItem().toString(); // Obținem numele tabelei
         Expression where = plainSelect.getWhere(); // Obținem clauza WHERE
 
-        if (where instanceof ComparisonOperator || where instanceof LikeExpression) {
-            processSingleCondition(rootDataBases, database, tableName, where);
-
-        } else if (where instanceof AndExpression) {
-            // Tratează cazul în care există mai multe condiții legate prin AND în clauza WHERE
-            List<Expression> conditions = extractMultipleConditions((AndExpression) where);
-            processMultipleConditions(rootDataBases, database, tableName, conditions);
+        if (where != null) {
+            List<Expression> conditions = extractConditions(where);
+            processConditions(rootDataBases, database, tableName, conditions);
         }
     }
 
-
-    private List<Expression> extractMultipleConditions(AndExpression andExpression) {
+    private List<Expression> extractConditions(Expression where) {
         List<Expression> conditions = new ArrayList<>();
-        Expression left = andExpression.getLeftExpression();
-        Expression right = andExpression.getRightExpression();
 
-        if (left instanceof AndExpression) {
-            conditions.addAll(extractMultipleConditions((AndExpression) left));
+        if (where instanceof AndExpression) {
+            AndExpression andExpression = (AndExpression) where;
+            conditions.addAll(extractConditions(andExpression.getLeftExpression()));
+            conditions.addAll(extractConditions(andExpression.getRightExpression()));
         } else {
-            conditions.add(left);
-        }
-
-        if (right instanceof AndExpression) {
-            conditions.addAll(extractMultipleConditions((AndExpression) right));
-        } else {
-            conditions.add(right);
+            conditions.add(where);
         }
 
         return conditions;
     }
 
-    private void processSingleCondition(Element rootDataBases, MongoDatabase database, String tableName, Expression where) throws Exception {
-        if (where instanceof ComparisonOperator) {
-            ComparisonOperator comparisonOperator = (ComparisonOperator) where;
-            Column column = (Column) comparisonOperator.getLeftExpression();
-            String columnName = column.getColumnName();
-
-            String conditionValue =  comparisonOperator.getRightExpression().toString();
-            List<String> matchingIDs = List.of();
-            String operator = comparisonOperator.getStringExpression();
-            if (existIndexForColumn(rootDataBases, tableName, columnName)) {
-                MongoCollection<Document> indexCollection = database.getCollection(dbmsRepository.getCurrentDatabase() + tableName + "Ind" + columnName);
-                matchingIDs = findMatchingIDs(indexCollection, columnName, conditionValue, operator);
-            }
-            if (!matchingIDs.isEmpty()) {
-                MongoCollection<Document> studentsCollection = database.getCollection(dbmsRepository.getCurrentDatabase() + tableName);
-                List<String> result = retrieveEntities(studentsCollection, matchingIDs);
-                result.forEach(System.out::println);
-            } else {
-                System.out.println("Nu au fost găsite înregistrări care să satisfacă toate condițiile.");
-            }
-        }else if( where instanceof LikeExpression){
-            LikeExpression likeExpression = (LikeExpression) where;
-            Column column = (Column) likeExpression.getLeftExpression();
-            String columnName = column.getColumnName();
-            String conditionValue =  likeExpression.getRightExpression().toString();
-            List<String> matchingIDs = List.of();
-            String operator = likeExpression.getStringExpression();
-
-            if (existIndexForColumn(rootDataBases, tableName, columnName)) {
-                MongoCollection<Document> indexCollection = database.getCollection(dbmsRepository.getCurrentDatabase() + tableName + "Ind" + columnName);
-                matchingIDs = findMatchingIDs(indexCollection, columnName, conditionValue, operator);
-            }
-            if (!matchingIDs.isEmpty()) {
-                MongoCollection<Document> studentsCollection = database.getCollection(dbmsRepository.getCurrentDatabase() + tableName);
-                List<String> result = retrieveEntities(studentsCollection, matchingIDs);
-                result.forEach(System.out::println);
-            } else {
-                System.out.println("Nu au fost găsite înregistrări care să satisfacă toate condițiile.");
-            }
-
-        }
-
-
-    }
-
-    private void processMultipleConditions(Element rootDataBases, MongoDatabase database, String tableName, List<Expression> conditions) throws Exception {
+    private void processConditions(Element rootDataBases, MongoDatabase database, String tableName, List<Expression> conditions) throws Exception {
         List<String> allMatchingIDs = new ArrayList<>();
 
         for (Expression condition : conditions) {
-            if (condition instanceof ComparisonOperator) {
-                ComparisonOperator comparisonOperator = (ComparisonOperator) condition;
-                Column column = (Column) comparisonOperator.getLeftExpression();
-                String columnName = column.getColumnName();
-
-                String conditionValue =  comparisonOperator.getRightExpression().toString();
-
-                String operator = comparisonOperator.getStringExpression();
-                if (existIndexForColumn(rootDataBases, tableName, columnName)) {
-                    MongoCollection<Document> indexCollection = database.getCollection(dbmsRepository.getCurrentDatabase() + tableName + "Ind" + columnName);
-                    List<String> matchingIDs = findMatchingIDs(indexCollection, columnName, conditionValue, operator);
-                    if (allMatchingIDs.isEmpty())
-                        allMatchingIDs.addAll(matchingIDs);
-                    else
-                        allMatchingIDs.retainAll(matchingIDs);
+            if (condition instanceof ComparisonOperator || condition instanceof LikeExpression) {
+                List<String> matchingIDs = processSingleCondition(rootDataBases, database, tableName, condition);
+                if (allMatchingIDs.isEmpty()) {
+                    allMatchingIDs.addAll(matchingIDs);
+                } else {
+                    allMatchingIDs.retainAll(matchingIDs);
                 }
             }
         }
 
         if (!allMatchingIDs.isEmpty()) {
-
             MongoCollection<Document> studentsCollection = database.getCollection(dbmsRepository.getCurrentDatabase() + tableName);
             List<String> result = retrieveEntities(studentsCollection, allMatchingIDs);
             result.forEach(System.out::println);
@@ -153,12 +83,47 @@ public class SelectService {
         }
     }
 
-    // Funcție pentru a verifica dacă există un index pentru o anumită coloană
-    private static boolean existIndexForColumn(Element rootDataBases, String tableName, String columnName) {
-        // Implementează logică pentru a verifica existența indexului pentru coloana specificată
-        // Parsează fișierul XML pentru a găsi detaliile despre indexe și verifică dacă există un index pentru coloana specificată
-        // Returnează true dacă există, altfel false
-        // Exemplu de implementare:
+    private List<String> processSingleCondition(Element rootDataBases, MongoDatabase database, String tableName, Expression where) throws Exception {
+        List<String> matchingIDs = new ArrayList<>();
+
+        if (where instanceof ComparisonOperator || where instanceof LikeExpression) {
+            Column column;
+            String conditionValue;
+            String operator;
+
+            if (where instanceof ComparisonOperator) {
+                ComparisonOperator comparisonOperator = (ComparisonOperator) where;
+                column = (Column) comparisonOperator.getLeftExpression();
+                conditionValue = comparisonOperator.getRightExpression().toString();
+                operator = comparisonOperator.getStringExpression();
+            } else {
+                LikeExpression likeExpression = (LikeExpression) where;
+                column = (Column) likeExpression.getLeftExpression();
+                conditionValue = likeExpression.getRightExpression().toString();
+                operator = likeExpression.getStringExpression();
+            }
+
+            String columnName = column.getColumnName();
+            List<String> tempMatchingIDs = new ArrayList<>();
+
+            if (existIndexForColumn(rootDataBases, tableName, columnName)) {
+                MongoCollection<Document> indexCollection = database.getCollection(dbmsRepository.getCurrentDatabase() + tableName + "Ind" + columnName);
+                tempMatchingIDs = findMatchingIDs(indexCollection, columnName, conditionValue, operator);
+            }
+
+            if (!tempMatchingIDs.isEmpty()) {
+                if (matchingIDs.isEmpty()) {
+                    matchingIDs.addAll(tempMatchingIDs);
+                } else {
+                    matchingIDs.retainAll(tempMatchingIDs);
+                }
+            }
+        }
+
+        return matchingIDs;
+    }
+
+     private static boolean existIndexForColumn(Element rootDataBases, String tableName, String columnName) {
         Element table = rootDataBases.getChild("DataBase").getChild("Table");
         String tableAttrName = table.getAttributeValue("tableName");
         if (tableAttrName.equals(tableName)) {
@@ -172,8 +137,7 @@ public class SelectService {
         return false;
     }
 
-    // Funcție pentru a găsi ID-urile care satisfac condiția specificată
-    private static List<String> findMatchingIDs(
+     private static List<String> findMatchingIDs(
             MongoCollection<Document> indexCollection,
             String columnName,
             String conditionValue,
@@ -205,8 +169,6 @@ public class SelectService {
                 query.append("_id", new Document("$regex", regexPattern));
                 break;
             default:
-                // Operatorul nu este recunoscut
-                // Poți trata această situație în funcție de necesitățile tale
                 break;
         }
         for (Document document : indexCollection.find(query)) {
@@ -216,16 +178,13 @@ public class SelectService {
         return matchingIDs;
     }
 
-    // Funcție pentru a recupera entitățile complete (înregistrările) pe baza ID-urilor
-    private static List<String> retrieveEntities(MongoCollection<Document> studentsCollection, List<String> matchingIDs) {
+     private static List<String> retrieveEntities(MongoCollection<Document> studentsCollection, List<String> matchingIDs) {
         List<String> entities = new ArrayList<>();
 
-        // Implementează logica de recuperare a entităților din colecția principală pe baza ID-urilor
-        // Exemplu de implementare:
         for (String id : matchingIDs) {
             Document document = studentsCollection.find(new Document("_id", id)).first();
             if (document != null) {
-                entities.add(document.toJson()); // sau procesează entitatea în alt mod
+                entities.add(document.toJson());
             }
         }
 
